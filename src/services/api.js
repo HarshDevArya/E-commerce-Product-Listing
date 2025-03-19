c7cbe46c7fb251db5b663594cfe74ff9e413f0b4
HarshDevArya <harshdevarya96@gmail.com>
2025-03-19 17:55:02 +0530

    cors problem

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,10 @@ export const fetchProducts = async (params = {}) => {
   console.log("this is params", params);
   const urlParams = new URLSearchParams(params).toString();
   const url = `${API_BASE_URL}/products/list.php?${urlParams}`;
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "cors", // <-- important if you want cors in some setups
+  });
   if (!response.ok) {
     throw new Error(`Network response was not ok: ${response.statusText}`);
   }
